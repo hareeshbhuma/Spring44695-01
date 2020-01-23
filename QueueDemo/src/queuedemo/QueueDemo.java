abb3cb7b70a9a895040ca01549d87f84962ad0d7
hareeshbhuma <54449210+hareeshbhuma@users.noreply.github.com>
2020-01-23 00:16:04 -0600

    Update QueueDemo.java

--- QueueDemo/src/queuedemo/QueueDemo.java
+++ QueueDemo/src/queuedemo/QueueDemo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package queuedemo;
+package queuedemoA;
 
 import java.util.LinkedList;
 import java.util.Queue;
